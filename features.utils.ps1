18425f09f662ef549e62936816f1b1c7c2445759
marcos.silvestrini <marcos.silvestrini@totvs.com.br>
2020-06-08 08:29:57 -0300

    Add class 4

--- features.utils.ps1
+++ features.utils.ps1
@@ -82,19 +82,21 @@ Function Get-Features {
 .FUNCTIONALITY
    Managment Windows Features with  Powershell
 #>
-Function Install-Features {
-    param(
-        [String]$ComputerName,
-        [String]$Name
-    )
-    Begin {
+function Install-WindowsFeatureInServers {
+   param(
+      [String[]] $computers,
+      [String] $featureName
+   )
 
+   $jobScriptBlock = {
+      param(
+         [String] $computerName,
+         [String] $featureName
+      )
+      Install-WindowsFeature -ComputerName $computerName -Name $featureName
+   }
 
-    }
-    Process {
-
-    }
-    End {
-
-    }
+   $computers | ForEach-Object {
+      Start-Job -Name "JOB_$_" -ScriptBlock $jobScriptBlock -ArgumentList ($_, $featureName)
+   }
 }
